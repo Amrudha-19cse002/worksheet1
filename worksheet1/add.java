c631c72eb88cfe96be4f21e979b119f4946f5a14
Amrudha-19cse002 <68490508+Amrudha-19cse002@users.noreply.github.com>
2020-07-31 18:08:46 -0700

    Add files via upload

---
+++ worksheet1/add.java
@@ -0,0 +1,16 @@
+package worksheet1;
+
+
+
+import java.util.*;
+public class add {
+    public static void main(String[]args){
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the value");
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        System.out.println("Addition"+(a+b));
+        
+    }
+    
+}
---
+++ worksheet1/random.java
@@ -0,0 +1,14 @@
+
+package worksheet1;
+
+/**
+ *
+ * @author Desktop Pc
+ */
+import java.util.*;
+public class random {
+    public static void main(String[] args)
+    {
+        System.out.println(int)(Math.random()*100);
+    }    
+}
